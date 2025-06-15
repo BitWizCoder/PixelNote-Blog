23af4badb15d06fa28a171945f2df696c2af7917
Muhammad Noman <mhmdnoman01@gmail.com>
2025-06-16 00:38:54 +0600

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import BlogPosts from "../components/BlogPosts";
@@ -8,7 +7,6 @@ import Cta from "../components/Cta";
 export default function Home() {
   return (
     <>
-      <Nav />
       <Hero />
       <BlogPosts />
       <Cta />

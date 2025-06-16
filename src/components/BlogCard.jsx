4960905d67ebc1fd19fbf8ea22dfb1395158b680
Muhammad Noman <mhmdnoman01@gmail.com>
2025-06-16 13:18:41 +0600

    added json server

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-export default function BlogCard() {
+export default function BlogCard({ post }) {
+  const excerpt =
+    post.body.length > 100 ? post.body.slice(0, 100) + "." : post.body;
+
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
@@ -13,12 +16,11 @@ export default function BlogCard() {
       <div className="p-5">
         <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            {post.title}
           </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          {excerpt}
         </p>
         <a
           href="#"
--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import BlogCard from "./BlogCard";
 import Button from "./Button";
+import { useState } from "react";
+import api from "../api/api";
+import { useEffect } from "react";
 
 export default function BlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUsers(); // Call the async function
+  }, []); // Empty dependency array = runs once on mount
+
+  if (loading) return <p>Loading...</p>;
+
+  console.log(posts);
+
   return (
     <div className="flex items-center flex-col justify-center gap-12">
       <div className="flex flex-wrap gap-8 items-center justify-center">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+        {posts.map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
       </div>
       <Button text="Read All" />
     </div>
